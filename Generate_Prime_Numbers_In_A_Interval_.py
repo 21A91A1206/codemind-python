c937a7923fc4db7aaa6e3adcbd870c30b209455d
21A91A1206 <98875470+21A91A1206@users.noreply.github.com>
2022-05-23 07:23:09 +0100

    s=int(input()) e=int(input()) for i in range(s,e+1):     if i>1:         for j in range(2,int(i**0.5)+1):             if i%j==0:                 break         else:             print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,9 @@
+s=int(input())
+e=int(input())
+for i in range(s,e+1):
+    if i>1:
+        for j in range(2,int(i**0.5)+1):
+            if i%j==0:
+                break
+        else:
+            print(i)
